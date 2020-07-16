f6106ed91537e8026a009ead979ae1fff6458987
Helder Sérvio <heldersrvio@gmail.com>
2020-07-16 15:22:26 -0300

    Fully functional. Styling missing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
+import WeatherCard from './components/WeatherCard';
+import SearchBar from './components/SearchBar';
 import './App.css';
 
-const API_KEY = 'c631b7482cbdada87723b245f6c46e0e';
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const App = () => {
-	const fetchWeatherInformation = async (location) => {
-		try {
-			return await fetch(
-				`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`
-			);
-		} catch (error) {
-			throw new Error('Not able to retrieve data from the server');
-		}
+  const [hasError, setHasError] = useState(false);
+  const [weather, setWeather] = useState({
+    weatherMain: "--",
+    weatherDescription: "--",
+    weatherIcon: "01d",
+    temperature: 273,
+    minimumTemperature: 273,
+    maximumTemperature: 273,
+    cityName: "--",
+  });
+
+	const setToDefaultWeatherInformation = async () => {
+    try {
+      const response = await fetchWeatherInformationWithCity('San Francisco');
+      const weatherInformation = await filterWeatherInformation(response);
+      setHasError(false);
+      setWeather(weatherInformation);
+    } catch(error) {
+      setHasError(true);
+    }
+	};
+
+	const setToUserLocation = async () => {
+    let weatherInformation;
+		if (navigator.geolocation) {
+			try {
+				const response = navigator.geolocation.getCurrentPosition(
+					fetchWeatherInformationWithCoordinates
+				);
+        weatherInformation = await filterWeatherInformation(response);
+        setHasError(false);
+        setWeather(weatherInformation);
+			} catch (error) {
+				setToDefaultWeatherInformation();
+			}
+		} else {
+			setToDefaultWeatherInformation();
+    }
+  };
+
+	const lookUpWeatherInformation = async (city) => {
+    try {
+      const response = await fetchWeatherInformationWithCity(city);
+      const weatherInformation = await filterWeatherInformation(response);
+      setHasError(false);
+      setWeather(weatherInformation);
+    } catch(error) {
+      setHasError(true);
+    }
+	};
+
+	const fetchWeatherInformationWithCoordinates = async (position) => {
+    return await fetch(
+      `api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${API_KEY}`, {
+        mode: 'cors'
+      }
+    );
+	};
+
+	const fetchWeatherInformationWithCity = async (city) => {
+    return await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`, {
+        mode: 'cors'
+      }
+    );
 	};
 
 	const filterWeatherInformation = async (response) => {
 		try {
-			const responseData = await response.json();
+      const responseData = await response.json();
 			return {
-				weatherMan: responseData.weather[0].main,
+				weatherMain: responseData.weather[0].main,
 				weatherDescription: responseData.weather[0].description,
+				weatherIcon: responseData.weather[0].icon,
 				temperature: responseData.main.temp,
 				minimumTemperature: responseData.main['temp_min'],
 				maximumTemperature: responseData.main['temp_max'],
@@ -28,9 +88,22 @@ const App = () => {
 		} catch (error) {
 			throw new Error('Not able to convert fetch to json');
 		}
-	};
+  };
 
-	return <div className="App"></div>;
+	return (
+		<div id="App" className={weather.weatherMain}>
+			<WeatherCard
+				weatherMain={weather.weatherMain}
+				weatherDescription={weather.weatherDescription}
+				weatherIcon={weather.weatherIcon}
+				temperature={weather.temperature}
+				minimumTemperature={weather.minimumTemperature}
+				maximumTemperature={weather.maximumTemperature}
+				cityName={weather.cityName}
+			/>
+      <SearchBar hasError={hasError} lookUpWeatherInformation={lookUpWeatherInformation} />
+		</div>
+	);
 };
 
 export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const SearchBar = (props) => {
+    const [inputValue, setInputValue] = useState('');
+    const errorMessage = (props.hasError)
+        ? <span>There was an error</span>
+        : null;
+    const handleSearchClick = () => {
+        props.lookUpWeatherInformation(inputValue);
+    };
+
+    return (
+        <div id="search-bar">
+            <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)}></input>
+            {errorMessage}
+            <div id="search-button">
+                <button onClick={handleSearchClick}>Search</button>
+            </div>
+        </div>
+    );
+};
+
+SearchBar.propTypes = {
+    hasError: PropTypes.bool,
+    lookUpWeatherInformation: PropTypes.func,
+};
+
+export default SearchBar;
