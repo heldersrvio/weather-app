7ce8f94c8344ec763a2ffe4ded8d550d11f2845b
Helder Sérvio <heldersrvio@gmail.com>
2020-07-15 22:17:05 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const fetchWeatherInformation = async (location) => {
+		try {
+			return await fetch(
+				`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=API_KEY`
+			);
+		} catch (error) {
+			throw new Error('Not able to retrieve data from the server');
+		}
+	};
+
+	const filterWeatherInformation = async (response) => {
+		try {
+			const responseData = await response.json();
+			return {
+				weatherMan: responseData.weather[0].main,
+				weatherDescription: responseData.weather[0].description,
+				temperature: responseData.main.temp,
+				minimumTemperature: responseData.main['temp_min'],
+				maximumTemperature: responseData.main['temp_max'],
+				cityName: responseData.sys.name,
+			};
+		} catch (error) {
+			throw new Error('Not able to convert fetch to json');
+		}
+	};
+
+	return <div className="App"></div>;
+};
 
 export default App;
